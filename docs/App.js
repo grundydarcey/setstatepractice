d91cdb558e42e2c07be43376eb9d944d1b4a41ca
Beyonce Knowles <you@example.com>
2020-12-15 21:41:24 -0500

    docs fix

---
+++ docs/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import HelloWorld from './state-drills/HelloWorld';
+
+class App extends React.Component {
+  render() {
+    return (
+    <HelloWorld>
+      <div></div>
+    </HelloWorld>
+  )}
+}
+
+export default App;
